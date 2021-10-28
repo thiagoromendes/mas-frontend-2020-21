2324b970a10886558655070e64f91dbd4cbd5e71
Thiago Mendes <thiagoromendes@gmail.com>
2021-10-28 20:43:48 -0300

    chore: add new routes to system

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import {BrowserRouter as Router} from 'react-router-dom'
+import Routes from '../src/routes';
+import GlobalStyle from "./styles/global";
 function App() {
+
   return (
-    <div className="App">
-      <h1>Frontend My Activies Space</h1>
-    </div>
+      <Router>
+        <Routes />
+        <GlobalStyle />
+      </Router>
   );
 }
 
